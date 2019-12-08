0b526c026f3ce64f99ec59126524496ea1581b9e
shaili shah <sshaili2912@gmail.com>
2019-12-07 21:56:06 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Team--Profile
+
+This application will use to generate team for the project.
+
+# Author
+Shaili Shah | 
+[LinkedIn](https://www.linkedin.com/in/shaili-shah-88706512b/) 
+| [GitHub](https://github.com/sshaili) 
+
+# Technologies
+
+* HTML
+* CSS 
+* JavaScript
+* JQuery
+* Node js
+
+# Repositories
+click here for Live Demo :  https://github.com/sshaili/Team--Profile
+
+# Assumption
+
+Node.js and node global packages installed in your environement.
+
+
+# NPM Dependencies
+
+* fs
+* inquirer
+* util
+
+# To run the application
+
+No 1. Run `npm install`.
+
+No 2. Run `node app.js`
+
+# Preview
+
+![](./Sample/Team-tracker.gif)
+
+# HTML Preview
+
+![image](https://user-images.githubusercontent.com/55032521/70383382-17b93500-193b-11ea-9957-2d4f75677e00.png)
+
+# Application testcase
+
+![](./Sample/Testcase.gif)
--- app.js
+++ app.js
@@ -1,33 +1,139 @@
-// Node modules
+
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require('util');
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
-// Lib modules
-const Engineer = require("./Application/Engineer");
-const Intern = require("./Application/Intern");
-const Manager = require("./Application/Manager");
+let employees = [];
 
-function validate(input) {
-    return input !== '';
-}
 
-function askQuestion(){
-    const questions = [
-        {
-            message: "Please choose the team member you want to add?",
+
+const questions = {
+    role : function() {
+        return {
+            message: "Which employee would you like to add to your team?",
             type: "list",
-            name: "member",
-            choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
-        },
-        {
-            message: `What is your ${member.toLowerCase()}'s ${item}?`,
-            type: "input",
-            name: "information",
-            validate: validate
+            name: "employee",
+            choices: [
+                "Engineer", 
+                "Intern", 
+                "I don't want to add anymore team members"
+            ]
         }
-    ]
+    },
+    information: function(employee, variable, information = variable) {
+        return {
+            message: `What is your ${employee}'s ${information}?`,
+            type: "input",
+            name: variable,
+            validate: function(value){
+                var string = value.match(/^\s*\S+.*/);
+                if (string) {
+                  return true;
+                } else {
+                  return "Please enter the information";
+                }
+            }
+        }
+    }
 };
 
 
-module.exports = askQuestion;
+async function makeTeam(employee) {
+    
+    let { id } = await inquirer.prompt(questions.information(employee, "id"));
+    let { name } = await inquirer.prompt(questions.information(employee, "name"));
+    let { email } = await inquirer.prompt(questions.information(employee, "email"));
 
+    switch (employee) {
+        
+        case "Manager":
+            let { officenumber } = await inquirer.prompt(questions.information(employee, "officenumber"));
+            employees.push(new Manager(name, id, email, officenumber));
+            break;
+            
+        case "Engineer":
+            let { github } = await inquirer.prompt(questions.information(employee, "github"));
+            employees.push(new Engineer(name, id, email, github));
+            break;
+
+        case "Intern":
+            let { school } = await inquirer.prompt(questions.information(employee, "school"));
+            employees.push(new Intern(name, id, email, school));
+            break;
+    }
+}
+
+
+function getHTMLModule(file) {
+    return readFile(file, "utf8");
+}
+
+
+async function generateHTML() {
+    let Template = {
+        Main: await getHTMLModule("./Templates/main.html"),
+        Manager: await getHTMLModule("./Templates/manager.html"),
+        Engineer: await getHTMLModule("./Templates/engineer.html"),
+        Intern: await getHTMLModule("./Templates/intern.html")
+    }
+
+    let htmlReview = "";
+
+    for (let employee of employees) {
+        let html = Template[employee.constructor.name]
+        .replace(/{id}/, employee.id)
+        .replace(/{name}/, employee.name)
+        .replace(/{email}/, employee.email);
+        switch (employee.constructor.name) {
+            case "Manager":
+                html = html.replace(/{officenumber}/, employee.officenumber);
+                break;
+            case "Engineer":
+                html = html.replace(/{github}/, employee.github);
+                break;
+            case "Intern":
+                html = html.replace(/{school}/, employee.school);
+                break;
+        }
+        htmlReview += html;
+    }
+    let completeHTML = Template["Main"].replace(/{employees}/, htmlReview);
+
+    createHTML(completeHTML);
+}
+
+
+async function createHTML(html) {
+    if (!fs.existsSync("./output")) {
+        fs.mkdirSync("./output");
+    }
+    await writeFile("./output/team.html", html);
+    console.log("HTML has been created to output.");
+    return;
+}
+
+
+async function init() {
+
+    await makeTeam("Manager");
+
+    let employee = "";
+    let exit = "I don't want to add anymore team members";
+
+    while (employee != exit) {
+        let { employee } = await inquirer.prompt(questions.role());
+
+        if (employee === exit) {
+            return generateHTML();
+        }
+
+        await makeTeam(employee);
+    }
+}
+
+init();
